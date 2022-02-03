0347a06336764ee7eefeaef541943cda8e178f6c
JCFarrow <36246083+JCFarrow@users.noreply.github.com>
2022-02-03 13:28:37 +1300

    Fix #255 Make audio of different sample rates combine correctly (#261)
    * Make audio of different frequencies combine correctly
    * Add link to FFmpeg documentation

--- main/commands/combineVideos.ts
+++ main/commands/combineVideos.ts
@@ -1,7 +1,5 @@
 import { spawnSync } from 'child_process';
 import { EventEmitter } from 'events';
-import { writeFileSync } from 'fs';
-import tmp from 'tmp-promise';
 import winston from 'winston';
 import { paths } from '../path-constants';
 
@@ -10,22 +8,20 @@ export async function combineVideos(
   outputFilePath: string,
   notifyEvent?: EventEmitter
 ): Promise<void> {
-  winston.info('Generating videoList file');
-  notifyEvent && notifyEvent.emit('Generating videoList file');
-  tmp.setGracefulCleanup();
-  // Create a temporary list file to concatenate videos (see https://trac.ffmpeg.org/wiki/Concatenate)
-  const { path: file, cleanup } = await tmp.file({ postfix: '.txt' });
-  const contents = videoPaths.map((videoPath) => `file '${videoPath}'`).join('\n');
-  writeFileSync(file, contents);
   winston.info('Combining Videos');
   notifyEvent && notifyEvent.emit('Combining Videos');
-  const combineProcess = spawnSync(
-    paths.ffmpeg,
-    ['-f', 'concat', '-loglevel', 'error', '-safe', '0', '-i', file, '-c', 'copy', outputFilePath],
-    {
-      stdio: 'pipe',
-    }
-  );
+  const args: string[] = ['-v', 'error'];
+  const filterComplex: string[] = [];
+  // create FFmpeg command to combine the videos (https://ffmpeg.org/ffmpeg-filters.html#concat)
+  for (let i = 0; i < videoPaths.length; i++) {
+    args.push('-i', videoPaths[i]);
+    filterComplex.push(`[${i}:0]`, `[${i}:1]`);
+  }
+  filterComplex.push(`concat=n=${videoPaths.length}:v=1:a=1`, '[v]', '[a]');
+  args.push('-filter_complex', filterComplex.join(' '), '-map', '[v]', '-map', '[a]', outputFilePath);
+  const combineProcess = spawnSync(paths.ffmpeg, args, {
+    stdio: 'pipe',
+  });
   const stderr = combineProcess.stderr.toString();
   if (stderr !== '') {
     winston.error(stderr);
@@ -34,5 +30,4 @@ export async function combineVideos(
     winston.info('Videos Combined');
     notifyEvent && notifyEvent.emit('Videos Combined');
   }
-  cleanup();
 }
